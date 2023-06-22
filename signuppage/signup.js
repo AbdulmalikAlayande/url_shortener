081803fd9fd29b3bf82c3aba9954ef31a6df58e9
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-06-22 20:57:05 +0100

    updated

--- signuppage/signup.js
+++ signuppage/signup.js
@@ -1,2 +1,22 @@
-let email = document.querySelector(".password")
-console.log(email)
+let form = document.getElementById('form');
+let emailInputBox = document.getElementsByName('email')
+let passwordInputBox = document.getElementById('password')
+
+console.log("Hello", "World");
+
+
+const isValidEmail = () => {
+    let regexPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    return false;
+}
+
+const isValidPassword = () => {
+    return false;
+};
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if (isValidEmail() && isValidPassword()){
+
+    }
+})
